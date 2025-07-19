ec3f4cdce847b801b374f6be8746e4e698f1cc51
Kshu27 <webflask27@gmail.com>
2025-07-19 22:34:11 +0530

    testing

--- backend/class_pred.py
+++ backend/class_pred.py
@@ -5,7 +5,7 @@ import cv2
 from tensorflow.keras.models import load_model
 
 model = load_model("backend\models\garbage_tf_model.h5")
-CLASS_NAMES = ["Biodegradable","Ewaste","hazardous","Non Biodegradable","Pharmaceutical and Biomedical Waste"]
+CLASS_NAMES = ["Biodegradable","Non Biodegradable","Ewaste","Pharmaceutical and Biomedical Waste","hazardous"]
 
 def classify_image(image_path):
     img = cv2.imread(image_path)
